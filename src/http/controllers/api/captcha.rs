0d72c80e99a5eacfd427ab2610a701f55df09bd0
流光 <67579722@qq.com>
2022-09-10 17:03:40 +0800

    fix captcha response

--- src/http/controllers/api/captcha.rs
+++ src/http/controllers/api/captcha.rs
@@ -1,9 +1,9 @@
 use crate::{
     common::ApiError,
-    http::{requests::SessionRequest, responses::CaptchaResponse},
+    http::requests::SessionRequest,
     services::{CaptchaService, SessionService},
 };
-use actix_web::{get, web};
+use actix_web::{get, http::header, web, CustomizeResponder, Responder};
 
 ///显示验证码图片
 #[get("/captcha/show")]
@@ -11,7 +11,7 @@ pub async fn show(
     session_service: web::Data<SessionService>,
     captcha_service: web::Data<CaptchaService>,
     query: web::Query<SessionRequest>,
-) -> Result<CaptchaResponse, ApiError> {
+) -> Result<CustomizeResponder<Vec<u8>>, ApiError> {
     let mut session = match session_service.load_session(&query.id).await? {
         Some(v) => v,
         None => return Err(ApiError::InvalidSessionID),
@@ -22,5 +22,13 @@ pub async fn show(
         .insert("code".to_string(), captcha.phrase.to_string());
     session_service.update_session(&session).await?;
     //show captcha
-    Ok(CaptchaResponse::new(captcha.data()))
+    let captcha_data = Vec::from(captcha.data());
+    let resp = captcha_data
+        .customize()
+        .insert_header((header::CONTENT_TYPE, "image/png"))
+        .insert_header((
+            header::CACHE_CONTROL,
+            "private, max-age=0, no-store, no-cache, must-revalidate",
+        ));
+    Ok(resp)
 }
--- src/http/responses.rs
+++ src/http/responses.rs
@@ -1,6 +1,4 @@
-mod captcha_response;
 mod init_session_response;
 mod login_response;
-pub use captcha_response::CaptchaResponse;
 pub use init_session_response::InitSessionResponse;
 pub use login_response::LoginResponse;
